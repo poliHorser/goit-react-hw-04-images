1071e55b8aefb797e5f0d788b40162a2821355f0
poliHorser <134537683+poliHorser@users.noreply.github.com>
2024-01-09 10:47:10 +0200

    start

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Button = ({ onClick, hasMoreImages }) => (
+  <>
+    {hasMoreImages && (
+      <button className="load-more-button" onClick={onClick}>
+        Load more
+      </button>
+    )}
+  </>
+);
+
+export default Button;
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,10 @@
+// Loader.jsx
+import React from 'react';
+
+const Loader = () => (
+  <div className="loader-container">
+    <p>Loading...</p>
+  </div>
+);
+
+export default Loader;
