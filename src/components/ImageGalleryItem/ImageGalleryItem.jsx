f4fd7e0f3d382fedaa054a2d5e3d490ec2bb3ade
poliHorser <134537683+poliHorser@users.noreply.github.com>
2024-01-09 10:51:13 +0200

    go

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react';
-
-// const ImageGalleryItem = ({ imageUrl, alt }) => (
-//   <li className="gallery-item">
-//     <img src={imageUrl} alt={alt} />
-//   </li>
-// );
-
-// export default ImageGalleryItem;
