7fd4b55f78a16a2b4ec870bc294807d0301270ae
poliHorser <134537683+poliHorser@users.noreply.github.com>
2024-01-09 12:32:26 +0200

    go

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { Component } from "react";
 import Searchbar from "./Searchbar/Searchbar";
 import fetchImages from './Api/Api';
-import ImageGallery from "./ImageGallery/ImageGallery";
+// import ImageGallery from "./ImageGallery/ImageGallery";
 class App extends Component{
     state = {
       images: [],
@@ -25,7 +25,7 @@ class App extends Component{
   };
   
   render() {
-     const { images, selectedImage, searchQuery } = this.state;
+    //  const { images, selectedImage, searchQuery } = this.state;
 
     return (
       <div>
