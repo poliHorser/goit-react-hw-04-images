3fc3b0beb3e388ee4389145a17f37d370cd95409
poliHorser <134537683+poliHorser@users.noreply.github.com>
2024-01-09 12:00:12 +0200

    w

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+// import React from 'react';
 
-const ImageGalleryItem = ({ imageUrl, alt }) => (
-  <li className="gallery-item">
-    <img src={imageUrl} alt={alt} />
-  </li>
-);
+// const ImageGalleryItem = ({ imageUrl, alt }) => (
+//   <li className="gallery-item">
+//     <img src={imageUrl} alt={alt} />
+//   </li>
+// );
 
-export default ImageGalleryItem;
+// export default ImageGalleryItem;
